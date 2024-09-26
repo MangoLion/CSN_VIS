9e1b4dcbfb18819ff14121f25c23daba332ec7b3
Brian Kim <briank1727@gmail.com>
2024-09-25 19:15:59 -0500

    Added Dividers and Changed Icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import "rc-dock/dist/rc-dock.css";
-import BarChart from "./PlotView";
 
 import "./styles/App.css";
 import "allotment/dist/style.css";
 
-import { Box, Drawer, IconButton } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { Box, Divider, Drawer, IconButton } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 import Uploader from "./Uploader/Uploader";
 import LineSegmentSettings from "./Line Segments/LineSegmentSettings";
@@ -19,55 +18,23 @@ import GraphCommunitiesRenderer from "./Graph Community/GraphCommunitiesRenderer
 const App = () => {
   const [open, setOpen] = useState(false);
 
-  const [swapLayout, setSwapLayout] = useState(false);
-  const [layerProps, setLayerProps] = useState({
-    x: 0,
-    y: 0,
-    scaleX: 1,
-    scaleY: 1,
-  });
-
-  const [canvasData, setCanvasData] = useState({});
-  const [settings, setSettings] = useState({});
-
-  const [selectRegion, setSelectRegion] = useState({
-    start: null,
-    end: null,
-  });
-
-  const [CSNG, setCSNG] = useState(false);
-  const [pixelMapData, setPixelMapData] = useState(false);
-
-  const [sphereRadius, setSphereRadius] = useState(1); // Default radius
-
-  const [selectionMode, setSelectionMode] = useState("Add");
-  const [shapeMode, setShapeMode] = useState("Sphere");
-  const [transformMode, setTransformMode] = useState("translate");
-
-  const [objFile, setObjFile] = useState(false);
-
-  const matRef = React.createRef();
-
-  //const amcsRef = useRef();
-
-  /**
-   * description
-   * @param {*} newProps
-   */
-  const handleLayerChange = (newProps) => {
-    setLayerProps(newProps);
-  };
-
   return (
-    <div className="App">
-      <IconButton onClick={() => setOpen(true)}>
-        <MenuIcon />
+    <div className="App" style={{ display: "flex" }}>
+      <IconButton
+        onClick={() => setOpen(true)}
+        sx={{ margin: 2, height: "40px" }}
+      >
+        <SettingsIcon />
       </IconButton>
+      <Divider orientation="vertical" flexItem />
       <Drawer open={open} onClose={() => setOpen(false)}>
         <Box sx={{ width: "800px" }}>
           <Uploader />
+          <Divider variant="middle" />
           <LineSegmentSettings />
+          <Divider variant="middle" />
           <NearestNeighborSettings />
+          <Divider variant="middle" />
           <GraphCommunitiesSettings />
         </Box>
       </Drawer>
